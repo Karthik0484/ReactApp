713745b54934e957c0bec27dd68d7090ab2b969f
Karthik0484 <karthikdeepshika484@gmail.com>
2026-03-17 21:25:45 +0530

    React app proper structure

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About(){
+    return(
+        <div>
+            <h3>About Page</h3>
+            <p>This is a simple React project using components</p>
+        </div>
+    );
+}
+
+export default About;
